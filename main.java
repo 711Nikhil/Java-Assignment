2fc2ef11264fe123282f3ed6ed3ae616d8556600
711Nikhil <114171176+711Nikhil@users.noreply.github.com>
2023-07-21 09:34:31 +0100

    Create main.java
    I left all the jframe comments in this main class even though most of it is in the Modelview class. Just makes it easier for me.

---
+++ main.java
@@ -0,0 +1,20 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        
+
+
+
+// JFrame adds GUI window that pops up
+/*JFrame jFrame = new JFrame("Window"); // instance of a JFrame, creates JFframe with "window" as title
+jFrame.setLayout(new FlowLayout()); // flowlayout is used to horizontally arrange buttons in a panel 
+jFrame.setSize(320,400); // setting size of the frame: x and y size dimensions
+jFrame.add(new JLabel("Outliner")); // text "Outliner" next to button - on the left of button
+jFrame.add(new JButton("Add Section")); // name of button - what the button is called "Add Section"
+jFrame.setVisible(true); //makes frame visible to be seen and pop up 
+jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // can be closed with the "cloe" button
+*/
+
+
+   new Modelview();
+}
+  }
