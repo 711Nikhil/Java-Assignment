f8be08e8f6b1726bf3e01171ffa1e41aac75c7df
711Nikhil <114171176+711Nikhil@users.noreply.github.com>
2023-07-21 09:36:12 +0100

    Update Modelview.java

--- Modelview.java
+++ Modelview.java
@@ -1,4 +1,26 @@
-public class Modelview {
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-  
+
+/**
+ *
+ * @author 44774
+ */
+public class Modelview extends JFrame{
+    
+   Modelview() {
+   
+   
+this.setLayout(new FlowLayout()); // flowlayout is used to horizontally arrange buttons in a panel 
+this.setSize(320,400); // setting size of the frame: x and y size dimensions
+this.add(new JLabel("Outliner")); // text "Outliner" next to button - on the left of button
+this.add(new JButton("Add Section")); // name of button - what the button is called "Add Section"
+this.setVisible(true); //makes frame visible to be seen and pop up 
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // can be closed with the "cloe" button
+   
+
+   }    
 }
+
